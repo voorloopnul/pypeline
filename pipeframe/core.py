4f52899bc0fe04f9f604a042d9bcc4ab03c6b9b7
Ricardo Pascal <voorloop@gmail.com>
2019-10-14 19:12:16 +0200

    add option to select between stream and batch sources
     - add placeholder feed function
     - add elapsed time

--- pipeframe/core.py
+++ pipeframe/core.py
@@ -1,3 +1,4 @@
+import time
 from multiprocessing import Process, Queue, cpu_count
 from queue import Empty
 
@@ -27,6 +28,10 @@ class PipelineEngine(Process):
                 break
         print("Finished {0} with {1} entries".format(self.name, count))
 
+    @staticmethod
+    def feed(bucket):
+        pass
+
 
 class PipeFrame(object):
 
@@ -35,7 +40,13 @@ class PipeFrame(object):
         self.output = Queue()
         self.cpu_count = _cpu_count
 
-    def run(self, _pipeline, load):
+    def run(self, _pipeline, load=None):
+        start_time = time.time()
+        load_function = load if load else _pipeline.feed
+
+        if _pipeline.load == 'batch':
+            load_function(self.stream)
+
         worker_list = []
         for i in range(self.cpu_count):
             worker = _pipeline(name="worker-{0}".format(i), stream=self.stream, output=self.output)
@@ -43,10 +54,15 @@ class PipeFrame(object):
 
         [worker.start() for worker in worker_list]
 
-        load(self.stream)
+        if _pipeline.load == 'stream':
+            load_function(self.stream)
 
         print("Waiting to join...")
         [worker.join() for worker in worker_list]
 
         print("Done!")
+        self.stream.cancel_join_thread()
+        end_time = time.time()
+        print("Elapsed time: {0}s".format(round(end_time-start_time, 2)))
+
         return self
